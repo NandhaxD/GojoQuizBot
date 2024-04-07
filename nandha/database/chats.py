c925bc82a401986b30c8fcc5793019a3c80c04b3
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-07 19:37:54 +0530

    Update chats.py

--- nandha/database/chats.py
+++ nandha/database/chats.py
@@ -1,1 +1,29 @@
+from nandha import DATABASE
 
+db = DATABASE['CHATS']
+
+
+async def get_chats() -> list:
+   chats_id = [chats['chat_id'] for chats in db.find()]
+   return chats_id
+  
+
+async def add_chat(chat_id: int):
+    json = {'chat_id': chat_id}
+    chats_id = await get_chats()
+    if not chat_id in chats_id:
+        db.insert_one(json)
+        return True
+    else:
+        return
+
+async def remove_chat(chat_id: int):
+     json = {'chat_id': chat_id}
+     is_exsited = db.find_one(json)
+     if is_exsited:
+           db.delete_one(json)
+           return True
+     else:
+         return 
+       
+  

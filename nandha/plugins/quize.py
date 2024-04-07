5407c63f4b9f7662b197bc2286c3e85619db7736
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-07 22:55:09 +0530

    Create quize.py

---
+++ nandha/plugins/quize.py
@@ -0,0 +1,27 @@
+import io
+import os
+import config
+import requests
+import asyncio
+import random 
+
+
+from nandha import bot
+from pyrogram import filters, enums
+from nandha.database.chats import add_chat
+
+
+@bot.on_message(filters.command('quize', prefixes=config.PREFIXES))
+@owner_only
+async def select_quize(_, message):
+      chat_id = message.chat.id
+      user_id = message.from_user.id
+
+      
+        
+                                  
+                                
+
+
+  
+     

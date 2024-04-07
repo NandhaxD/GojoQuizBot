13f48d53f48244a6335b60f4f96dec3e11ae0535
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-07 16:44:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ SUPPORT_URL = 'nandhasupport.t.me'
 CHANNEL_URL = 'nandhabots.t.me'
 PREFIXES = ['.','?','!','/','~']
 
+START_IMAGE = 'https://graph.org/file/15a7cc4a22cad1f6189f0.jpg'
+
 # checks if the env is true then program takes variable from environment else here.
 if bool(os.getenv('ENV')) == True:
      OWNER_ID = int(os.getenv('OWNER_ID'))

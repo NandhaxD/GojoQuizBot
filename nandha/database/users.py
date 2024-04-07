bb6fcc615c5af16c20b9cfe962f3acad3266217e
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-07 14:05:16 +0530

    Update users.py

--- nandha/database/users.py
+++ nandha/database/users.py
@@ -1,1 +1,25 @@
+from nandha import DATABASE
 
+db = DATABASE['USERS']
+
+
+async def get_users() -> list:
+    users_id = [ user['user_id'] for user in db.find()]
+    return users_id
+
+async def add_user(user_id: int):
+    json = {'user_id': user_id}
+    users_id = await get_users()
+    if user_id in users_id:
+           return
+    else:
+        db.insert_one(json)
+        return True
+
+async def remove_user(user_id: int):
+      json = {'user_id': user_id}
+      users_id = await get_users()
+      if user_id in users_id:
+          db.delete_one(json)
+          return True
+      return 

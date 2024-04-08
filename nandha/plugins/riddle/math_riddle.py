0ad06bde8c9bf0b18dc3b37b17313aa337ddd848
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-08 16:03:36 +0530

    Update math_riddle.py

--- nandha/plugins/riddle/math_riddle.py
+++ nandha/plugins/riddle/math_riddle.py
@@ -1,1 +1,33 @@
 
+
+
+from pyrogram import filters 
+from nandha.database.riddle.math_riddle import is_chat_riddle, get_chat_time
+from nandha import bot
+
+
+
+@bot.on_callback_query(filters.regex('^rmath'))
+async def riddle_math(_, query):
+      user_id = query.from_user.id
+      chat_id = query.message.chat.id
+      
+      admin_id = int(query.data.split(':')[1])
+      if user_id != admin_id:
+            return await query.answer("🔐 Sorry this not for you. try you're own to customize.", show_alert=True)
+      else:
+         await add_chat(chat_id)
+         riddle = await is_chat_riddle(chat_id)
+         time = await get_chat_time(chat_id)
+         button = [[
+           InlineKeyboardButton(text='60 seconds', callback_data='mrtime:60'),
+           InlineKeyboardButton(text='1 hour ', callback_data='mrtime:3600'),],
+                   [InlineKeyboardButton(text='3 hours', callback_data='mrtime:10800'),
+                    InlineKeyboardButton(text='6 hours', callback_data='mrtime:21600'), ],
+                   [ InlineKeyboardButton(text='back ⬅️', callback_data='mrriddle')
+           
+]]
+         return await query.message.edit(
+           f"Hello! Now you can set up a time for your chat. Click the button below to do so.\n\n <b> You're current chat riddle: {riddle}\nYou're current chat time: {time}",
+           reply_markup=InlineKeyboardMarkup(button))
+       

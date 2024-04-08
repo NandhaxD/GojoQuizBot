9793916773aef247722e439b379e8c4605d60b35
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-08 15:05:26 +0530

    Update math_riddle.py

--- nandha/database/riddle/math_riddle.py
+++ nandha/database/riddle/math_riddle.py
@@ -5,10 +5,25 @@ from nandha import DATABASE
 
 db = DATABASE['CHATS']
 
-async def get_chat_timeline(chat_id: int):
+
+
+async def get_chat_switch(chat_id: int):
+    json = {'chat_id': chat_id}
+    riddle = db.find_one(json)
+    if riddle:
+        if bool(riddle['data']['riddle']['math']['switch']) is True:
+             return True
+        else:
+             return False
+    else:
+        return False
+
+
+async def get_chat_time(chat_id: int):
     json = {'chat_id': chat_id}
     riddle = db.find_one(json)
     if riddle:
         return riddle['data']['riddle']['math']['time']
     else:
         return False
+        

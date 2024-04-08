8e784a4c83b373153b87474dbf092c279d2bb8f8
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-09 01:23:41 +0530

    Update func.py

--- nandha/helpers/func.py
+++ nandha/helpers/func.py
@@ -3,7 +3,7 @@ import random
 
 async def get_question():     
      symbol = ['+','-','*']
-     question = "({num1}{syb1}{num2}){syb2}{num3}".format(num1=randint(20, 44), syb1=choice(symbol), num2=randint(2, 9), syb2=choice(symbol), num3=randint (1, 30))     
+     question = "({num1}{syb1}{num2}){syb2}{num3}".format(num1=random.randint(20, 44), syb1=random.choice(symbol), num2=random.randint(2, 9), syb2=random.choice(symbol), num3=random.randint (1, 30))     
      answer = eval(query)
      return {'question': question, 'answer': answer}
 

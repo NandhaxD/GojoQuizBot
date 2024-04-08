e8fde8428350cd5423447f3798b9eac0972fbae1
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-08 09:49:39 +0530

    Update quize.py

--- nandha/plugins/quize.py
+++ nandha/plugins/quize.py
@@ -11,12 +11,22 @@ from pyrogram import filters, enums
 from nandha.database.chats import add_chat
 from nandha.helpers.decorator import admin_only
 
+
+button = [[
+      InlineKeyboardButton(text='Customize', callback_data='customize'),
+      InlineKeyboardButton(text='Skip', callback_data='skip')
+      
+]]
+
 @bot.on_message(filters.command('quize', prefixes=config.PREFIXES))
 @admin_only
 async def select_quize(_, message):
       chat_id = message.chat.id
       user_id = message.from_user.id
-
+      await message.reply(
+            "Before setting up a quiz in your group, you need to establish a timeline for sending the quiz periodically, with breaks in between. Click the button below to set up the quiz timeline."
+      , reply_markup=InlineKeyboardMarkup(button), quote=True)
+                        
       
         
                                   

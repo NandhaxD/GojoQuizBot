35de133f7e925cc2e623ab3d426a7560460a8f0a
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-08 17:38:41 +0530

    Update math_riddle.py

--- nandha/database/riddle/math_riddle.py
+++ nandha/database/riddle/math_riddle.py
@@ -11,12 +11,9 @@ async def is_chat_riddle(chat_id: int):
     json = {'chat_id': chat_id}
     riddle = db.find_one(json)
     if riddle:
-        if bool(riddle['data']['riddle']['math']['switch']) is True:
-             return True
-        else:
-             return False
+         return riddle['data']['riddle']['math']['switch']        
     else:
-        return False
+        return None
 
 
 async def get_chat_time(chat_id: int):
@@ -25,5 +22,5 @@ async def get_chat_time(chat_id: int):
     if riddle:
         return riddle['data']['riddle']['math']['time']
     else:
-        return False
+        return None
         

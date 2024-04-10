be1b4b2a67e27119e79596d5ff6f50484413c6ef
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-10 22:37:39 +0530

    Update users.py

--- nandha/database/users.py
+++ nandha/database/users.py
@@ -3,17 +3,29 @@ from nandha import DATABASE
 db = DATABASE['USERS']
 
 
+    
 async def get_users() -> list:
     users_id = [ user['user_id'] for user in db.find()]
     return users_id
 
 async def add_user(user_id: int):
-    json = {'user_id': user_id}
+    json_data = {
+    'user_id': user_id,
+    'data': {
+        'riddle': {
+            'math': { '-1001717881477': 0 }                        
+         },
+        
+         'quiz': {
+             'math': { '-1001717881477': 0 }
+         }        
+    }
+    }
     users_id = await get_users()
     if user_id in users_id:
            return
     else:
-        db.insert_one(json)
+        db.insert_one(json_data)
         return True
 
 async def remove_user(user_id: int):

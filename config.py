3c8c13e63d120c86d47c879a0e67a9f6398e8166
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-12 00:18:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,18 @@ SUPPORT_URL = 'nandhasupport.t.me'
 CHANNEL_URL = 'nandhabots.t.me'
 PREFIXES = ['@','.','?','!','/','~']
 
+
 START_IMAGE = 'https://graph.org/file/15a7cc4a22cad1f6189f0.jpg'
+
+RIDDLE_MATH_BG = [
+     "https://graph.org/file/9b165baf9de57406d76ca.jpg",
+     "https://graph.org/file/bd790b253ac3befdcff51.jpg",
+     "https://graph.org/file/8ce59f8e42399029e9051.jpg",
+     "https://graph.org/file/010811dae00123d74ad41.jpg"
+]
+
+
+
 
 # checks if the env is true then program takes variable from environment else here.
 if bool(os.getenv('ENV')) == True:

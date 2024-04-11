cf47eaa122a7ed546ef20c027b1a16f3c8010237
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-11 17:16:02 +0530

    Update points.py

--- nandha/database/points.py
+++ nandha/database/points.py
@@ -29,7 +29,7 @@ async def add_point_user_chat(chat_id: int, user_id: int, module: str, type: str
         filter = {'user_id': user_id}
         user = db.find_one(filter)
         if user:
-            point = await get_point_user_chat(chat_id, user_id)
+            point = await get_point_user_chat(chat_id, user_id, module, type)
             point += 1
             update = {'$set':
                       {

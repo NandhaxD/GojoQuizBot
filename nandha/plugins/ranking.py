a1b269272ede04b5fec2e45a3a2b1b8ca692b83a
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-18 00:09:29 +0530

    Update ranking.py

--- nandha/plugins/ranking.py
+++ nandha/plugins/ranking.py
@@ -3,21 +3,20 @@ import config
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from nandha.helpers.decorator import admin_only
-from nandha.helpers.func import get_rmath_lb
+from nandha.helpers.leaderboard import get_rmath_top, get_rmath_gtop
 from nandha.database.chats import add_chat
 from nandha import bot
 
 
 
-@bot.on_message(filters.command('leaderboard', prefixes=config.PREFIXES) & ~filters.private)
-@admin_only
+@bot.on_message(filters.command('top', prefixes=config.PREFIXES) & ~filters.private)
 async def leaderboard(_, message):
          user_id = message.from_user.id
          chat_id = message.chat.id
          
          button = [[
-                InlineKeyboardButton('Riddle', callback_data=f'riddlelb:{user_id}'),
-                InlineKeyboardButton('Quiz', callback_data=f'quizelb:{user_id}')
+                InlineKeyboardButton('Riddle', callback_data=f'riddletop:{user_id}'),
+                InlineKeyboardButton('Quiz', callback_data=f'quiztop:{user_id}')
          ]]
          await add_chat(chat_id)
          return await message.reply(
@@ -26,8 +25,8 @@ async def leaderboard(_, message):
          )
 
 
-@bot.on_callback_query(filters.regex('^riddlelb'))
-async def riddlelb(_, query):
+@bot.on_callback_query(filters.regex('^riddletop'))
+async def riddletop(_, query):
        user_id = query.from_user.id
        admin_id = int(query.data.split(':')[1])
 
@@ -38,19 +37,20 @@ async def riddlelb(_, query):
        else:
                 
             button = [[
-             InlineKeyboardButton('R-M Ranks', callback_data=f'rmathlb:{user_id}')
+             InlineKeyboardButton('R-M Group Users 👥', callback_data=f'rmathtop:{user_id}'),
+             InlineKeyboardButton('R-M Global Users 👥', callback_data=f'rmathgtop:{user_id}')
                    
             ]]
             name = query.message.chat.title
             return await query.message.edit(
-                   f'Click Here For Know The Chat Top Users in {name}.🏆',
+                   f'Click Here For Know Global Top / Group Top Users',
                    reply_markup=InlineKeyboardMarkup(button)
             )
             
        
                      
-@bot.on_callback_query(filters.regex('^rmathlb'))
-async def rmath_leaderboard(_, query):
+@bot.on_callback_query(filters.regex('^rmathtop'))
+async def rmath_top(_, query):
        user_id = query.from_user.id
        admin_id = int(query.data.split(':')[1])
        if user_id != admin_id:
@@ -69,7 +69,31 @@ async def rmath_leaderboard(_, query):
            return await query.message.edit(text,
                                     reply_markup=InlineKeyboardMarkup(button)
                                           )
-       
+
+
+@bot.on_callback_query(filters.regex('^rmathgtop'))
+async def rmath_gtop(_, query):
+       user_id = query.from_user.id
+       admin_id = int(query.data.split(':')[1])
+       if user_id != admin_id:
+              return await query.answer(
+                     'This command is not requested by you', show_alert=True
+              )
+       else:
+           chat_id = query.message.chat.id
+           sorted_user_riddle_points = await get_rmath_lb(chat_id)
+           text = f'🏆 **Global Top R-M Users** 👥\n\n'
+           sorted_leaderboard = await get_rmath_gtop()
+           for i, (user_id, point) in enumerate(sorted_leaderboard.items()):
+                  if i >= 10:
+                     break
+                  text += f'{i+1}, [{user_id}](tg://user?id={user_id}): {point}'
+
+           button = [[ InlineKeyboardButton('Back ⬅️', callback_data=f'riddletop:{user_id}') ]]
+           return await query.message.edit(text,
+                                    reply_markup=InlineKeyboardMarkup(button)
+                                          )
+
 
        
 

c7eef4b40b606285c1d65eceb3f10b98f7811e51
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-19 11:17:26 +0530

    Update leaderboard.py

--- nandha/helpers/leaderboard.py
+++ nandha/helpers/leaderboard.py
@@ -9,14 +9,15 @@ async def get_rmath_gtop():
      for user in all_users:
          if 'data' in user and 'riddle' in user['data'] and 'math' in user['data']['riddle']:
               for chat_id, points in user['data']['riddle']['math'].items():
-                   try:
-                        name = user['data']['first_name']
+                   
+                        name = user['data']['first_name'] if user['data'] and user['data']['first_name'] else None
                         leaderboard[user['user_id']] += points
-                   except:    
-                        leaderboard[user['user_id']] += points
-
+                        pre_points = leaderboard[user['user_id']]
+                        leaderboard[user['user_id']] = {'first_name': name, 'points': pre_points}
+                        
+                   
      # Sort the leaderboard by points
-     sorted_leaderboard = dict(sorted(leaderboard.items(), key=lambda x: x[1], reverse=True))
+     sorted_leaderboard = dict(sorted(leaderboard.items(), key=lambda x: x['points'], reverse=True))
      return sorted_leaderboard
   
      
@@ -31,10 +32,9 @@ async def get_rmath_top(chat_id: str):
             data = user_data['data']
             if 'riddle' in data and 'math' in data['riddle'] and str(chat_id) in data['riddle']['math']:
                   points = data['riddle']['math'][str(chat_id)]
-                  try:
-                      name = data['first_name']
-                      user_points[user_id] = points
-                  except:
-                      user_points[user_id] = points
-       sorted_user_points = sorted(user_points.items(), key=lambda x: x[1], reverse=True)
+                  
+                      name = data['first_name'] if data and data['first_name'] else None
+                      user_points[user_id] = {'first_name': name, 'points': pre_points}
+                  
+       sorted_user_points = sorted(user_points.items(), key=lambda x: x['points'], reverse=True)
        return sorted_user_points

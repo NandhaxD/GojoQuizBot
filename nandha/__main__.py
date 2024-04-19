169e34fa5c8e7b788ec29686ecfffcc244584664
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-19 16:43:43 +0530

    Update __main__.py

--- nandha/__main__.py
+++ nandha/__main__.py
@@ -1,13 +1,8 @@
 
 from nandha import bot
 
-async def main():
-    await bot.start()
-    print('The bot has now started.')
-    
-    info = await bot.get_me()
-    bot_username = info.username
-    bot_id = info.id
 
 if __name__ == "__main__":
-    bot.loop.run_until_complete(main())
+    bot.run()
+    print('The bot has now started.')
+    

14882c0b22bad437056df758f78d1e7fbcb88662
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-19 12:20:36 +0530

    Update leaderboard.py

--- nandha/helpers/leaderboard.py
+++ nandha/helpers/leaderboard.py
@@ -2,6 +2,9 @@
 from collections import defaultdict
 from nandha import DATABASE
 
+
+
+
 async def get_rmath_gtop():
      db = DATABASE['USERS']
      all_users = db.find({})
@@ -11,8 +14,7 @@ async def get_rmath_gtop():
               for chat_id, points in user['data']['riddle']['math'].items():
                  name = user['data']['first_name'] if user['data'] and user['data']['first_name'] else None
                  leaderboard[user['user_id']] += points
-                 pre_points = leaderboard[user['user_id']]
-                 leaderboard[user['user_id']] = {'first_name': name, 'points': pre_points}             
+                              
      # Sort the leaderboard by points
      sorted_leaderboard = dict(sorted(leaderboard.items(), key=lambda x: x['points'], reverse=True))
      return sorted_leaderboard
@@ -26,7 +28,7 @@ async def get_rmath_top(chat_id: str):
             if 'riddle' in data and 'math' in data['riddle'] and str(chat_id) in data['riddle']['math']:
                   points = data['riddle']['math'][str(chat_id)]
                   name = data['first_name'] if data and data['first_name'] else None
-                  user_points[user_id] = {'first_name': name, 'points': points}
+                  user_points[user_id] = points
                   
        sorted_user_points = sorted(user_points.items(), key=lambda x: x['points'], reverse=True)
        return sorted_user_points

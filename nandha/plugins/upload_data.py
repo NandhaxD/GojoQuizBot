6c067cfbd7aacc8c36ff9cb0674c9710bfa4655f
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-04-28 08:19:55 +0530

    Update upload_data.py

--- nandha/plugins/upload_data.py
+++ nandha/plugins/upload_data.py
@@ -27,7 +27,10 @@ async def upload_data(_, message):
     chat_id = message.chat.id 
     mention = message.from_user.mention
     user_id = message.from_user.id
-   
+
+    
+   if user_id in data:
+      return await message.reply('Already one question in process please wait.')
     # /upload -q {question} -1 {option1} -2 {option2} -3 {option3} -4 {option4} -a {answer}
     try:
         text = format_data(message.text)
@@ -73,6 +76,4 @@ async def upload_data(_, message):
         is_anonymous=False
     )):
        
-       if user_id in data:
-           return await message.reply('Already one question in process please wait.')
        data[user_id] = text

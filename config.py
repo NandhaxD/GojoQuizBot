65a6b761597c3df9a09032851cd0565308158f8c
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-11 11:27:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ else:
 DEVS_ID = [ 5456798232 ] + [OWNER_ID]
      
 NAME = 'GojoQuizeBot'
-SUPPORT_URL = 'nandhasupport.t.me'
-CHANNEL_URL = 'nandhabots.t.me'
+SUPPORT_URL = 'NandhaSupport.t.me'
+CHANNEL_URL = 'Nandhabots.t.me'
 GROUP_ID =  -1002039454048
 PREFIXES = ['/', '\\']
 EMOJI = ['❤️', '⚡️', '😈', '👍', '🔥']

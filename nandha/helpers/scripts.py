de05981a5fedcec87e1102dd044e677b26bc5691
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-06-10 15:05:36 +0530

    Update scripts.py

--- nandha/helpers/scripts.py
+++ nandha/helpers/scripts.py
@@ -1,10 +1,24 @@
 
 
 from pyrogram import filters, types, enums, errors
+from nandha.database.users import get_users
 from nandha import bot
 
 import random
+import config
 
+
+async def ask_start_pm(user_id: int, message):
+    users = await get_users()    
+    if not user_id in users:
+         button = [[
+           types.InlineKeyboardButton(text='✨ Start ✨', url=f'{config.NAME}.t.me?start=True')]]
+         await message.reply(
+             f'**⛔ Hello, {message.from_user.mention} start the bot in private and then start answering in {message.chat.title} 💫**',
+             reply_markup=types.InlineKeyboardMarkup(button))
+         return False
+    else:
+         return True
 
 
 async def react(message):
